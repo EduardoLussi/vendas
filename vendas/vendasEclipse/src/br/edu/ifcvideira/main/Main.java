f9791c7095e743fa88b7b4da71099987b8f3ec70
duduplaygames <eduardolussi@gmail.com>
2018-08-21 08:07:33 -0300

    Criação da Compra

--- vendas/vendasEclipse/src/br/edu/ifcvideira/main/Main.java
+++ vendas/vendasEclipse/src/br/edu/ifcvideira/main/Main.java
@@ -1,11 +1,18 @@
 package br.edu.ifcvideira.main;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
+
+import br.edu.ifcvideira.beans.Compras;
+import br.edu.ifcvideira.beans.Produtos;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Produtos> p = new ArrayList();
+		ArrayList<Compras> c = new ArrayList();
 		
 		for (int i = 0; i == 0;) {
 			
@@ -20,7 +27,31 @@ public class Main {
 			
 			if (op == 1) {
 				
+				Produtos prod = new Produtos();
+				Compras compra = new Compras();
 				
+				prod.setCd_produto(Integer.parseInt(JOptionPane.showInputDialog("Digite o código do produto")));
+				
+				if (p.size() < prod.getCd_produto()) {
+					JOptionPane.showMessageDialog(null, "Produto inexistente, ele será adicionado com o código " + (int) (p.size() + 1));
+					prod.setCd_produto(p.size() + 1);
+					prod.setNm_produto(JOptionPane.showInputDialog("Digite o nome do produto:\n"));
+					prod.setVl_produto(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de venda do produto\n")));
+					prod.setQt_estoque_produto(0);
+					p.add(prod);
+				}
+				
+				prod = p.get(prod.getCd_produto() - 1);
+				
+				compra.setProdutoCompra(prod);
+				
+				compra.setVl_compra(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da compra do produto\n")));
+				compra.setQt_comprada_compra(Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade comprada\n")));
+				
+				prod.setQt_estoque_produto(prod.getQt_estoque_produto() + compra.getQt_comprada_compra());
+				
+				p.set(prod.getCd_produto() - 1, prod);
+				c.add(compra);
 				
 			} else if (op == 2) {
 				
